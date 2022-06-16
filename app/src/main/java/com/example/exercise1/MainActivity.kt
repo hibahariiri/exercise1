a01de53294f7f1f682a2e16eda8b43d85ea85b77
hibahariiri <78786591+hibahariiri@users.noreply.github.com>
2022-06-16 11:13:36 +0200

    123

--- app/src/main/java/com/example/exercise1/MainActivity.kt
+++ app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -4,10 +4,12 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -20,16 +22,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.exercise1.ui.theme.InstantSOStheme
-import com.example.exercise1.ui.theme.Orange500
-import com.example.exercise1.ui.theme.Purple200
-import com.example.exercise1.ui.theme.Teal200
+import com.example.exercise1.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            baggrund()
             eksempel1()
+            
         }
 
             /*InstantSOStheme() {
@@ -50,24 +51,19 @@ class MainActivity : ComponentActivity() {
 
 }
 
-    @Composable
-    fun GetButton(
+@Composable
+fun baggrund(){
+   Column(
+       Modifier
+           .background(darkgrey)
+           .fillMaxSize()
+           .padding(8.dp)
+   ) {
 
-        text:String,
-        onClick: ()-> Unit,
-    ){
-        Button(
-            onClick = onClick,
-            shape = RoundedCornerShape(4.dp)
-            
-        ){
-            Text(
-                text = text,
-                Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+   } 
 
-            )
-        }
-    }
+}
+
 
 @Composable
 fun eksempel1() {
@@ -77,53 +73,56 @@ fun eksempel1() {
             .fillMaxWidth()
             .absolutePadding(10.dp, 50.dp, 10.dp, 0.dp), horizontalAlignment = Alignment.CenterHorizontally){
 
-         Text(text = "Tryk på knap ud fra niveau af akutsituation", fontSize = 17.sp, fontWeight = FontWeight.Bold)
+         Text(text = "Tryk på knap ud fra niveau af akutsituation", fontSize = 17.sp, fontWeight = FontWeight.Bold, color = Hvid)
 
 
 
-        Spacer(modifier = Modifier.height(60.dp))
+        Spacer(modifier = Modifier.height(40.dp))
         Box() {
             Button(
                 onClick = { /*TODO*/ },
+                shape = CircleShape,
                 colors = ButtonDefaults.buttonColors(backgroundColor = androidx.compose.ui.graphics.Color.Red),
                 modifier = Modifier
-                    .height(120.dp)
-                    .width(240.dp)
+                    .size(150.dp)
+
                 )
              {
                 Text(
-                    text = "Nødknap 1", fontSize = 20.sp)
+                    text = "Nødknap 1", fontSize = 20.sp, color = Hvid)
                 Modifier.padding(12.dp)
 
             }
         }
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(25.dp))
         Box() {
             Button(
                 onClick = { /*TODO*/ },
+                shape = CircleShape,
                 colors = ButtonDefaults.buttonColors(backgroundColor = Orange500),
                 modifier = Modifier
-                    .height(120.dp)
-                    .width(240.dp)
+                    .size(150.dp)
+
+
                 )
                 {
                 Text(
-                    text = "Nødknap 2", fontSize = 20.sp)
+                    text = "Nødknap 2", fontSize = 20.sp, color = Hvid )
                 Modifier.padding(12.dp)
 
             }
         }
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(25.dp))
         Box() {
             Button(
                 onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(backgroundColor = androidx.compose.ui.graphics.Color.Yellow),
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(backgroundColor = Gul),
                 modifier = Modifier
-                    .height(120.dp)
-                    .width(240.dp)
+                    .size(150.dp)
             ) {
                 Text(
-                    text = "Nødknap 3", fontSize = 20.sp)
+                    text = "Nødknap 3", fontSize = 20.sp, color = Hvid)
                 Modifier.padding(40.dp)                 
 
             }
@@ -146,3 +145,7 @@ fun PreviewMessageCard() {
         MessageCard("Android")
     }
 }}
+
+
+
+
--- app/src/main/java/com/example/exercise1/ui/theme/Color.kt
+++ app/src/main/java/com/example/exercise1/ui/theme/Color.kt
@@ -8,4 +8,7 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Orange500 = Color(0xFFFF781F)
+val darkgrey = Color(0xFF3D3D3D)
+val Hvid = Color(0xFFFFFFFF)
+val Gul = Color(0xFFF6BE00)
 
